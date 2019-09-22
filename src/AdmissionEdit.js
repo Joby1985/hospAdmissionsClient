376e7c2894ef61f43e4c3559ec677d51cdd2afc3
Joby <joby.job@unico.com.au>
2019-09-23 09:19:27 +1000

    Fixed few issues in edit

--- src/AdmissionEdit.js
+++ src/AdmissionEdit.js
@@ -11,12 +11,12 @@ class AdmissionEdit extends Component {
   };
 
   emptyItem = {
-    name: '',
-    address: '',
-    city: '',
-    stateOrProvince: '',
-    country: '',
-    postalCode: ''
+	admissionId: '',
+    patientName: '',
+    patientDOB: '',
+    patientSex: '',
+    admissionDate: '',
+    category: ''
   };
 
   constructor(props) {
@@ -33,7 +33,7 @@ class AdmissionEdit extends Component {
   async componentDidMount() {
     if (this.props.match.params.id !== 'new') {
       try {
-        const admission = await (await fetch(`/admissions/${this.props.match.params.id}`, {credentials: 'include'})).json();
+        const admission = await (await fetch(`/admissions`, {credentials: 'include'})).json();
         this.setState({item: admission});
       } catch (error) {
         this.props.history.push('/');
@@ -76,38 +76,26 @@ class AdmissionEdit extends Component {
       <Container>
         {title}
         <Form onSubmit={this.handleSubmit}>
+		  <FormGroup>
+            <Label for="name">AdmissionID</Label>
+            <Input type="text" name="id" id="id" value={item.admissionId || ''}
+                   onChange={this.handleChange} autoComplete="admissionId"/>
+          </FormGroup>
           <FormGroup>
             <Label for="name">Name</Label>
-            <Input type="text" name="name" id="name" value={item.name || ''}
-                   onChange={this.handleChange} autoComplete="name"/>
+            <Input type="text" name="name" id="name" value={item.patientName || ''}
+                   onChange={this.handleChange} autoComplete="patientName"/>
           </FormGroup>
           <FormGroup>
-            <Label for="address">Address</Label>
-            <Input type="text" name="address" id="address" value={item.address || ''}
-                   onChange={this.handleChange} autoComplete="address-level1"/>
+            <Label for="DOB">DOB</Label>
+            <Input type="Date" name="DOB" id="DOB" value={item.patientDOB || ''}
+                   onChange={this.handleChange} autoComplete="patientDOB"/>
           </FormGroup>
           <FormGroup>
-            <Label for="city">City</Label>
-            <Input type="text" name="city" id="city" value={item.city || ''}
-                   onChange={this.handleChange} autoComplete="address-level1"/>
+            <Label for="Sex">Sex</Label>
+            <Input type="select" name="Sex" id="Sex" value={item.patientSex || ''}
+                   onChange={this.handleChange} autoComplete="patientSex"/>
           </FormGroup>
-          <div className="row">
-            <FormGroup className="col-md-4 mb-3">
-              <Label for="stateOrProvince">State/Province</Label>
-              <Input type="text" name="stateOrProvince" id="stateOrProvince" value={item.stateOrProvince || ''}
-                     onChange={this.handleChange} autoComplete="address-level1"/>
-            </FormGroup>
-            <FormGroup className="col-md-5 mb-3">
-              <Label for="country">Country</Label>
-              <Input type="text" name="country" id="country" value={item.country || ''}
-                     onChange={this.handleChange} autoComplete="address-level1"/>
-            </FormGroup>
-            <FormGroup className="col-md-3 mb-3">
-              <Label for="country">Postal Code</Label>
-              <Input type="text" name="postalCode" id="postalCode" value={item.postalCode || ''}
-                     onChange={this.handleChange} autoComplete="address-level1"/>
-            </FormGroup>
-          </div>
           <FormGroup>
             <Button color="primary" type="submit">Save</Button>{' '}
             <Button color="secondary" tag={Link} to="/groups">Cancel</Button>
